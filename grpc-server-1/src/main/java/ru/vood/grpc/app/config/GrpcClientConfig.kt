773e05a21a54419f6e4994d2def387c025caab10
vood <balitsky_igor@list.ru>
2022-02-04 22:04:32 +0300

    2q

--- grpc-server-1/src/main/java/ru/vood/grpc/app/config/GrpcClientConfig.kt
+++ grpc-server-1/src/main/java/ru/vood/grpc/app/config/GrpcClientConfig.kt
@@ -3,12 +3,14 @@ package ru.vood.grpc.app.config
 import net.devh.boot.grpc.client.inject.GrpcClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import ru.vood.grpc.app.proto.SimpleGrpc
+import ru.vood.grpc.app.proto2.Simple2Grpc.Simple2BlockingStub
 
 @Configuration
 open class GrpcClientConfig {
 
-    @Bean
     @GrpcClient("srv2")
-    open fun sad(sad: SimpleGrpc.SimpleBlockingStub) = sad
+    lateinit var sad: Simple2BlockingStub
+
+    @Bean
+    open fun sad1()/*(sad: Simple2Grpc.Simple2BlockingStub)*/ = sad
 }
--- grpc-server-1/src/main/java/ru/vood/grpc/app/config/GrpcServer.kt
+++ grpc-server-1/src/main/java/ru/vood/grpc/app/config/GrpcServer.kt
@@ -7,15 +7,19 @@ import org.slf4j.LoggerFactory
 import ru.vood.grpc.app.GlobalGrpcExceptionHandler
 import ru.vood.grpc.app.proto.HelloRequest
 import ru.vood.grpc.app.proto.SimpleGrpc.SimpleImplBase
+import ru.vood.grpc.app.proto2.HelloRequest2
+import ru.vood.grpc.app.proto2.Simple2Grpc.Simple2BlockingStub
 
 @GrpcService
-class GrpcServer : SimpleImplBase(), SyncResponseSender<Empty> {
+class GrpcServer(val client: Simple2BlockingStub) : SimpleImplBase(), SyncResponseSender<Empty> {
 
     private val LOGGER = LoggerFactory.getLogger(GlobalGrpcExceptionHandler::class.java)
 
     override fun sayHello(request: HelloRequest, responseObserver: StreamObserver<Empty>) {
-        super.sayHello(request, responseObserver)
-        sendOk(responseObserver, Empty.getDefaultInstance())
+
+        LOGGER.info("sayHello ${request.name}")
+        val hello = client.getHello(HelloRequest2.newBuilder().setName(request.name).build())
+        sendOk(responseObserver, hello)
 
     }
 
