888c0d58658dd0ea99cdafba62eab202cd57ac01
vood <balitsky_igor@list.ru>
2022-02-07 12:11:42 +0300

    2q

--- grpc-server-1/src/main/java/ru/vood/grpc/GrpcApplication.java
+++ grpc-server-1/src/main/java/ru/vood/grpc/GrpcApplication.java
@@ -2,8 +2,10 @@ package ru.vood.grpc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GrpcApplication {
 
     public static void main(String[] args) {
---
+++ grpc-server-1/src/main/java/ru/vood/grpc/app/Scheduler.kt
@@ -0,0 +1,7 @@
+package ru.vood.grpc.app
+
+import org.springframework.stereotype.Service
+
+@Service
+class Scheduler {
+}
